1396495b458363bd95c4e4518ea4896c85d16ed9
itsrocky1 <89401635+itsrocky1@users.noreply.github.com>
2023-10-02 03:21:06 +0800

    Update chart.js

--- static/chart.js
+++ static/chart.js
@@ -3,7 +3,7 @@ var chart = Highcharts.stockChart('container', {
         selected: 1
     },
     data: {
-        csvURL: window.origin + '/static/data.csv'
+        csvURL: 'https://itsrocky1.github.io/temg4950l-dashboard/static/data.csv'
     },
     tooltip: {
         valueDecimals: 2
